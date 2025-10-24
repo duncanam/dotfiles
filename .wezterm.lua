86d6312ea702682b4a1a8f0972d906e951a7460d
Duncan McGough <22781288+duncanam@users.noreply.github.com>
2025-10-23 17:15:31 -0700

    Adding wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Visual
+config.color_scheme = 'Catppuccin Mocha'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = 'NeverPrompt'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16.0
+
+-- and finally, return the configuration to wezterm
+return config
