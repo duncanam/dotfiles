3a7c4bf4d2d39f3ad83562ff606e0ef73dacb166
Duncan McGough <22781288+duncanam@users.noreply.github.com>
2025-11-04 17:09:48 -0800

    Correcting LSP configuration for Python

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "python", "rust" }
+local servers = { "html", "cssls", "pyright", "rust_analyzer" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
