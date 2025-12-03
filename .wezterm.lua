384f8a9e5189b351a4e06a398cb972027f3321ce
Duncan McGough <22781288+duncanam@users.noreply.github.com>
2025-12-03 11:07:23 -0800

    Reducing terminal padding

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration
 local config = {}
@@ -7,19 +7,27 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Visual
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_close_confirmation = 'NeverPrompt'
-config.font = wezterm.font 'JetBrains Mono'
+config.window_close_confirmation = "NeverPrompt"
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16.0
 
 -- and finally, return the configuration to wezterm
 config.keys = {
-  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
+}
+
+-- Remove padding to have better use of screen real estate
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
